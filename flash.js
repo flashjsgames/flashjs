d972686ad5060d8b9eba4f32d4e022aca454bcbd
Jining Liu <88249105+JiningLiu@users.noreply.github.com>
2022-10-05 22:23:13 +0000

    Initial commit

---
+++ flash.js
@@ -0,0 +1,27 @@
+
+// Copyright Jining Liu. MIT License. More on GitHub.
+
+var flashjs = (function () {
+
+  var config
+
+  function init(configuration) {
+    const aspectRatio = configuration.aspectRatio;
+    aspectRatio = aspectRatio.split(':');
+    if (aspectRatio.length != 2) {
+      console.error('flashjs: variable aspectRatio must be a valid ratio. (Example: 1:1)');
+      return;
+    }
+    const aspectRatioInt = [];
+    aspectRatio.forEach((val) => {
+      aspectRatioInt.push(val.parseInt());
+    });
+    aspectRatio = aspectRatioInt;
+    alert(aspectRatio);
+  }
+  
+  return {
+    init: init,
+  }
+  
+})();
